f7808f6ea66a26616b3c5e65ecb60f5988cd24e0
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2023-04-05 16:00:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,32 +2,50 @@ import java.util.Scanner;
 public class Main
 {
     static Setup setup = new Setup();
+    static Display display = new Display();
+    
     public static void main(String[] args)
     {
+        display.displayWelcome();
+        
         Scanner scan = new Scanner(System.in);
-        int choice =0 ;
+        int numPlayers = 0;
+        int choice = 0;
         String name;
         int turn;
         int turnCounter;
-
-
-        System.out.println("Please enter the Number of Players playing 2-4");
-        int numPlayers = scan.nextInt();
+        
         //Add Error handling incase number inputed is not between 2 and 4
-
-        for(int x = 0; x<numPlayers; x++)
+        while (numPlayers < 2 || numPlayers > 4) {
+            System.out.println("Please enter the Number of Players playing 2-4");
+            try {
+                numPlayers = scan.nextInt();
+                scan.nextLine(); // do this to clear input buffer 
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number between2 and 4.");
+            }
+        }
+       
+        for(int x = 1; x<=numPlayers; x++)
         {
-            System.out.println("Please enter your name");
-            name =scan.nextLine();
-            User user = new User(name, x+1);
+            System.out.println("Please enter player " + x + "'s name");
+            name = scan.nextLine();
+            User user = new User(name, x);
             setup.addUser(user);
             //Add users
         }
 
+        turnCounter = setup.turnCalculation();
 
         while(choice!=3)
         {
+            User currentUser = setup.getUserByTurn(turnCounter);
             System.out.println("");
+            System.out.println("It is " + currentUser.getName() + "'s turn.");
+            System.out.println("What would you like to do?");
+            System.out.println("1. Play turn");
+            System.out.println("2. Next player");
+            System.out.println("3. Quit");
             choice = scan.nextInt();
 
             //Print out the name of the current persons turn and what turn it is
@@ -36,28 +54,20 @@ public class Main
                 case 1: //Play turn
 
                 case 2: //Skip turn
-
-
-                case 3: // Exit
-                    choice =3;
+                    turnCounter = (turnCounter % numPlayers) + 1;
                     break;
 
+                case 3: // Exit
+                    display.displayQuit();
+                    break;
+                    
+                default:
+                    display.displayCommandNotPossible();
+                    break;
             }
-
-
-
+            
         }
 
-
-
-
     }
-
-
-int turnCalculation()
-{
-//Do the turn Calculation Function to randomise each player turn based on the number of players inputted
-    return 0;
-}
 
 }
