9dfe1c149e09d0ae6278d55a79e378828442bd44
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2023-04-06 18:37:54 +0100

    Update Setup.java

--- Setup.java
+++ Setup.java
@@ -9,7 +9,8 @@ public class Setup
     void addUser(User user)
     {
         userList.add(user);
-    
+    }
+
     User getUserByTurn(int turn) {
         for (User user : userList) {
             if (user.getTurn() == turn) {
@@ -18,24 +19,45 @@ public class Setup
         }
         return null;
     }
-    
+
+    // Calculates turn order randomly
     int turnCalculation() {
         List<Integer> turns = new ArrayList<>();
         for (int i = 1; i <= userList.size(); i++) {
             turns.add(i);
         }
+        // Shuffle the turn numbers and return the first one
         Collections.shuffle(turns);
         return turns.get(0);
     }
-    
-    ArrayList<TilesAndTokens> setStarterHabitatTile() {
+
+    // Define a list of possible habitat tiles
+    private ArrayList<TilesAndTokens> habitats() {
         ArrayList<TilesAndTokens> habitatTiles = new ArrayList<>();
-        // add all available starter tiles to the list
+
         habitatTiles.add(TilesAndTokens.FOREST);
         habitatTiles.add(TilesAndTokens.WETLAND);
         habitatTiles.add(TilesAndTokens.RIVER);
         habitatTiles.add(TilesAndTokens.MOUNTAIN);
         habitatTiles.add(TilesAndTokens.PRAIRIE);
+        return habitatTiles;
+    }
+
+    // Define a lis of possible wildlife tokens
+    private ArrayList<TilesAndTokens> wildlife() {
+        ArrayList<TilesAndTokens> wildlifeTokens = new ArrayList<>();
+
+        wildlifeTokens.add(TilesAndTokens.HAWK);
+        wildlifeTokens.add(TilesAndTokens.BEAR);
+        wildlifeTokens.add(TilesAndTokens.ELK);
+        wildlifeTokens.add(TilesAndTokens.SALMON);
+        wildlifeTokens.add(TilesAndTokens.FOX);
+        return wildlifeTokens;
+    }
+
+    // Return the starter tile for each user
+     ArrayList<TilesAndTokens> setStarterHabitatTile() {
+        ArrayList<TilesAndTokens> habitatTiles = habitats();
         // shuffle the list and select the first two objects as the user's starter tile
         Collections.shuffle(habitatTiles);
         ArrayList<TilesAndTokens> starterTiles = new ArrayList<>();
@@ -44,13 +66,33 @@ public class Setup
         return starterTiles;
     }
 
+    // Return shuffled habitat tiles
+     ArrayList<TilesAndTokens> habitatTiles() {
+         ArrayList<TilesAndTokens> habitatTiles = habitats();
+         for (int i = 0; i < habitatTiles.size(); i++) {
+             Collections.shuffle(habitatTiles);
+        }
+        return habitatTiles;
+    }
+
+    // Return shuffled wildlife tokens
+    ArrayList<TilesAndTokens> wildlifeTokens() {
+        ArrayList<TilesAndTokens> wildlifeTokens = wildlife();
+        for (int i = 0; i < wildlifeTokens.size(); i++) {
+            Collections.shuffle(wildlifeTokens);
+        }
+        return wildlifeTokens;
+    }
+
+
+
     private void showUserTiles(String name)
     {
         for(User user: userList)
         {
             if(user.getName().equals(name))
             {
-                System.out.println();//Supposed to pass through the current tokens and tiles each person has
+                //Supposed to pass through the current tokens and tiles each person has
             }
         }
     }
