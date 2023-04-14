c4e27c8727740dc8f82ce9067eb12c5a973d7dff
maryannailo <104728553+maryannailo@users.noreply.github.com>
2023-04-14 12:27:30 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,29 +3,40 @@ import java.util.Arrays;
 public class Board {
 
     public static void printBoard(String[][][] board) {
+        int numRows = board.length;
+        int numCols = board[0].length;
+
         // Print column labels
         System.out.print("   ");
-        for (int col = 0; col < board[0].length; col++) {
-            System.out.print(" " + col + "   ");
+        for (int col = 0; col < numCols; col++) {
+            System.out.printf("%-5s", col);
         }
-        System.out.println(); // Print newline after column labels
+        System.out.println();
 
-        for (int row = 0; row < board.length; row++) {
-            // Print row label
-            System.out.print(row + " ");
-
-            for (int col = 0; col < board[row].length; col++) {
-                // Print top left character of tile
-                System.out.print(" " + board[row][col][0]);
+        // Print board with row labels
+        for (int row = 0; row < numRows; row++) {
+            System.out.printf("%-2s", row);
+            for (int col = 0; col < numCols; col++) {
+                if (board[row][col][0] == null) {
+                    System.out.print("     ");
+                } else {
+                    System.out.printf("%-5s", board[row][col][0]);
+                }
             }
-            System.out.println(); // Print newline after each row
+            System.out.println();
         }
     }
-
-
+    public static void placeTile(String[][][] board, String[][] tile, int row, int col) {
+        for (int i = 0; i < tile.length; i++) {
+            for (int j = 0; j < tile[i].length; j++) {
+                if (board[row + i][col + j][0] == null){
+                    board[row + i][col + j][0] = tile[i][j];
+                }else{
+                    System.out.println("Error: There is already a tile at this location.");
+                }
+            }
+        }
+    }
+ 
+    
 }
-
-//first the column labels isnt outputting for all coloumns
-//the rest should not be outputting null
-// trying to figure out how to put the tiles (2d arrays) inside the board at the right positions
-//also swapping the colours on the tile
