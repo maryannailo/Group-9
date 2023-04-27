be48dc00b758583668733a0fa6f43877c41d6284
salsekri <124359779+salsekri@users.noreply.github.com>
2023-04-27 16:30:36 +0100

    Update Scoring.java

--- Scoring.java
+++ Scoring.java
@@ -20,7 +20,7 @@ public class Scoring {
                 "FOX SCORE CARD (A)\n" +
                         "Score points shown for each Fox, depending on the number of unique wildlife types adjacent to it. Other adjacent Foxes may be scored as unique when scoring each fox",
                 "FOX SCORE CARD (B)\n" +
-                        "Score points shown for each Fox, depending on the number of unique pairs of the other wildlife types adjacent to it. Other adjacent Fox pairs do not score. Each pair of other wildlife must be uniqure- the same wildlife may not count for more than one pair, even if four or six of that wildlife are adjacent. Pairs of wildlife do not need to be adjacent to each other.",
+                        "Score points shown for each Fox, depending on the number of unique pairs of the other wildlife types adjacent to it. Other adjacent Fox pairs do not score. Each pair of other wildlife must be uniquer- the same wildlife may not count for more than one pair, even if four or six of that wildlife are adjacent. Pairs of wildlife do not need to be adjacent to each other.",
                 "FOX SCORE CARD (C)\n" +
                         "Score points for each Fox, depending on the number of a single wildlife type adjacent to it. Other adjacent Foxes may not be score."};
         System.out.println(fScore[random.nextInt(3)]);
@@ -37,7 +37,7 @@ public class Scoring {
                 "ELK SCORE CARD (B)\n" +
                         "Score points shown for each contiguous group of adjacent Elk in any shape, depending on size of group.",
                 "ELK SCORE CARD (C)\n" +
-                        "Score points shown for each group of Elk in each exact shape. Two groups of Elk may be ajacent to one another, howeverm each Elk may only count fora single group. Groups may be rotated."};
+                        "Score points shown for each group of Elk in each exact shape. Two groups of Elk may be adjacent to one another, however each Elk may only count fora single group. Groups may be rotated."};
 
         System.out.println(eScore[random.nextInt(3)]);
     }
@@ -50,21 +50,24 @@ public class Scoring {
                 "HAWK SCORE CARD (B)\n" +
                         "Score points shown for each hawk, based on it's line of sight to any other Hawks that are not directly adjacent. Line of sight is not interrupted by a gap in Habitat Tiles, or by other wildlife.",
                 "HAWK SCORE CARD (C)\n" +
-                        "Score 3 points for each pair of Hawks that share a direct line of sight between them. Each Hawk may only be counted as part of multiple pairs. Line of sight is not interrupter by a gap ion habitatl tiles, or by other wildlife."};
+                        "Score 3 points for each pair of Hawks that share a direct line of sight between them. Each Hawk may only be counted as part of multiple pairs. Line of sight is not interrupter by a gap ion habitat tiles, or by other wildlife."};
 
         System.out.println(hScore[random.nextInt(3)]);
     }
 
     public static void salmonScore()
     {
-        String[] sScore=new String[] {
+        String[] sScore=new String[] 
+                {
                 "SALMON SCORE CARD (A)\n" +
-                        "Score poitns shown for each run of Salmon, depending on length of run. A run is defined as a group of adjacent Salmon whhere each Salmon is adjacent to no more than two other Salmon. A gorup of three Salmon in a traingle shape may count as a run, but no other Salmon may be attached to this run. Each run of Salmon may not have any other Salmon adjacent to it.",
+                        "Score points shown for each run of Salmon, depending on length of run. A run is defined as a group of adjacent Salmon where each Salmon is adjacent to no more than two other Salmon. A group of three Salmon in a triangle shape may count as a run, but no other Salmon may be attached to this run. Each run of Salmon may not have any other Salmon adjacent to it.",
                 "SALMON SCORE CARD (B)\n" +
-                        "Score points shown for each run of Salmon, depending on length of run. A run is defined as a gorup of adjacent Salmon where each Salmon is adjacent to no more than two other Salmon. A group of three salmon in a traingle shape may count as a run, but no other Salmon amy vbe attacked to shi run. Each run of Salmon may not have any other Salmon adjacent to it.",
-};
+                        "Score points shown for each run of Salmon, depending on length of run. A run is defined as a group of adjacent Salmon where each Salmon is adjacent to no more than two other Salmon. A group of three salmon in a triangle shape may count as a run, but no other Salmon amy vbe attacked to shi run. Each run of Salmon may not have any other Salmon adjacent to it.",
+                "SALMON SCORE CARD (C)\n" +
+                        "Score points shown for each Fox, depending on the number of a single wildlife type adjacent to it. Other adjacent Foxes may not be scored.",
+        };
 
-        System.out.println(sScore[random.nextInt(2)]);
+        System.out.println(sScore[random.nextInt(3)]);
     }
 
 }
