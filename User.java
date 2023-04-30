7dd19e6bb409dd4cbc0472b67e9dd0adf225c99a
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2023-05-01 00:33:56 +0100

    Update User.java

--- User.java
+++ User.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class User
 {
     private String name;
     private int turn;
+    ArrayList<TilesAndTokens> userNatureTokens = new ArrayList<>();
+    ArrayList<TilesAndTokens> natureTokens = Setup.natureTokens();
+
     //Add the tokens here as well
 
     User(String name, int turn)
@@ -24,5 +30,22 @@ public class User
 
     public void setTurn(int turn) {
         this.turn = turn;
+    }
+
+    public void addNatureToken() {
+        int index = new Random().nextInt(natureTokens.size());
+
+        //add first element of the nature tokens to the current user's nature tokens
+        userNatureTokens.add(natureTokens.get(index));
+        natureTokens.remove(index);
+    }
+
+    public void natureTokensCount() {
+        System.out.println(getName() + "'s nature tokens: " + userNatureTokens.size());
+    }
+
+    public  void spendToken() {
+        int index = new Random().nextInt(userNatureTokens.size());
+        userNatureTokens.remove(index);
     }
 }
