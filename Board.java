4c4d42b1b290ea8892cf9c7f6b0df5fd07603f6b
maryannailo <104728553+maryannailo@users.noreply.github.com>
2023-05-02 20:38:56 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,12 +1,6 @@
-//This class represents the game board in which Habitat Tiles and Tokens will be displayed
-//
-
 import java.util.Arrays;
 
 public class Board {
-
-//this method shows a grid of the board, making it easier for the play to place their tiles and tokens in the specific place
-
     public static void printBoard(String[][][] board) {
         int numRows = board.length;
         int numCols = board[0].length;
@@ -22,7 +16,7 @@ public class Board {
         for (int row = 0; row < numRows; row++) {
             System.out.printf("%-2s", row);
             for (int col = 0; col < numCols; col++) {
-                if (board[row][col][0] == null) {
+                if (null == board[row][col][0]) {
                     System.out.print("     ");
                 } else {
                     System.out.printf("%-5s", board[row][col][0]);
@@ -31,8 +25,6 @@ public class Board {
             System.out.println();
         }
     }
-    
-    //this will place the tile onto the board. Each tile is located in the HabitatTiles class
     public static void placeTile(String[][][] board, String[][] tile, int row, int col) {
         for (int i = 0; i < tile.length; i++) {
             for (int j = 0; j < tile[i].length; j++) {
@@ -44,10 +36,14 @@ public class Board {
             }
         }
     }
-    
+
+
+
+
+
     //this will replace the potential token in the string and make it the actual token
     //chosen by the player
-    public static String replace (String[][] arr, String oldSubstr, String newSubstr){
+    private static String replace (String[][] arr, String oldSubstr, String newSubstr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (arr[i][j].contains(oldSubstr)) {
@@ -58,37 +54,72 @@ public class Board {
         return Arrays.toString(arr);
     }
 
+    //This is for the nature token, this will check if a tile has a nature token inside, when the player places a token onto the tile
+    private static boolean checkSequence2DString(String[][] tileChosen){
+        String sequence = new String(String.valueOf(TilesAndTokens.KEY.getSymbol()));
+        for (int i = 0; i < tileChosen.length; i++) {
+            for (int j = 0; j < tileChosen[i].length; j++) {
+                if (tileChosen[i][j].contains(sequence)) {
 
-//this method replaces the letter representation of the token with an emoticon/emoji, this will make it easier for the player to distinguish what token has been placed onto the tile. 
-    public static void placeToken(String[][][] board,String [][] tileChosen ,int row, int col, String oldStr) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void placeToken(String[][][] board,String [][] tileChosen ,int row, int col, String oldStr, User user) {
         String[][] tile = new String[][]{board[row][col]};
-        String newStr = new String();
+        String newStr = null;
+
         for (int i = 0; i < tile.length; i++) {
             for (int j = 0; j < tile[i].length; j++) {
-                switch (oldStr){
+                switch (oldStr) {
                     case "E" -> {
                         newStr = String.valueOf(TilesAndTokens.ELKTOKEN.getSymbol());
-                        replace(tileChosen,oldStr,newStr);
+                        replace(tileChosen, oldStr, newStr);
+                        if (checkSequence2DString(tileChosen) == true) {
+                            System.out.println("Nature Token added");
+                            user.addNatureToken();
+                        }
                         break;
                     }
                     case "F" -> {
                         newStr = String.valueOf(TilesAndTokens.FOXTOKEN.getSymbol());
-                        replace(tileChosen,oldStr,newStr);
+                        replace(tileChosen, oldStr, newStr);
+                        if (checkSequence2DString(tileChosen) == true) {
+                            System.out.println("Nature Token added");
+                            user.addNatureToken();
+                        }
                         break;
                     }
                     case "H" -> {
                         newStr = String.valueOf(TilesAndTokens.HAWKTOKEN.getSymbol());
-                        replace(tileChosen,oldStr,newStr);
+                        replace(tileChosen, oldStr, newStr);
+                        if (checkSequence2DString(tileChosen) == true) {
+                            System.out.println("Nature Token added");
+                            user.addNatureToken();
+                        }
                         break;
                     }
                     case "S" -> {
                         newStr = String.valueOf(TilesAndTokens.SALMONTOKEN.getSymbol());
-                        replace(tileChosen,oldStr,newStr);
+                        replace(tileChosen, oldStr, newStr);
+                        if (checkSequence2DString(tileChosen) == true) {
+                            System.out.println("Nature Token added");
+                            user.addNatureToken();
+                        }
                         break;
                     }
                     case "B" -> {
                         newStr = String.valueOf(TilesAndTokens.BEARTOKEN.getSymbol());
-                        replace(tileChosen,oldStr,newStr);
+                        replace(tileChosen, oldStr, newStr);
+                        if (checkSequence2DString(tileChosen) == true) {
+                            System.out.println("Nature Token added");
+                            user.addNatureToken();
+                        }
                         break;
                     }
                     default -> System.out.println("Cannot place this token onto the tile");
@@ -96,4 +127,19 @@ public class Board {
             }
         }
     }
+
+    public static String[][] swapColors(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j].contains(DisplayColour.LIGHTGREEN) && j < 3) {
+                    array[i][j] = array[i][j].replace(DisplayColour.LIGHTGREEN, DisplayColour.YELLOW);
+                } else if (array[i][j].contains(DisplayColour.YELLOW) && j > 3) {
+                    array[i][j] = array[i][j].replace(DisplayColour.YELLOW, DisplayColour.LIGHTGREEN);
+                }
+            }
+        }
+        return array;
+    }
+
 }
+
